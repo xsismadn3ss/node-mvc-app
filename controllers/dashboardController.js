14cae63641ea264df18cf779278cf819102558c4
xs_ismand3ss <2477carlos.a@gmail.com>
2024-11-15 21:43:40 -0600

    implementar controlador para la vista de dashboard

--- controllers/dashboardController.js
+++ controllers/dashboardController.js
@@ -1,1 +1,14 @@
 const {getProductsWithCategory} = require('./modules/queries')
+
+async function dashboardView(req, res){
+    const products = await getProductsWithCategory()
+    if(products){
+        return res.render('dashboard', {products})
+    }else{
+        return res.render('dashboard')
+    }
+}
+
+module.exports = {
+    dashboardView
+}
