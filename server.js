770ef9ba30cd59f4344ba085d8c0d2b3101a8bb3
xs_ismand3ss <2477carlos.a@gmail.com>
2024-11-15 21:45:23 -0600

    añadir rutas adicionales al archivo principal

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const express = require("express");
 const db = require("./config/db");
 const homeRoutes = require("./routes/home");
 const categoryRoutes = require("./routes/category");
-const ProductRoutes = require("./routes/product");
+const productRoutes = require("./routes/product");
+const dashboardRoutes = require('./routes/dashboard')
 
 const app = express();
 const PORT = process.env.PORT;
@@ -14,18 +15,21 @@ app.set("view engine", "pug");
 // usar rutas
 app.use("/", homeRoutes);
 app.use("/", categoryRoutes);
-app.use("/", ProductRoutes);
+app.use("/", productRoutes);
+app.use('/', dashboardRoutes)
 
 // sincronizar base de datos
 db.sync({ force: false })
   .then(() => {
     app.listen(
       PORT,
+      console.clear(),
       console.log("\nServidor corriendo en: http://localhost:" + PORT),
       console.log("hot reload activado...\n"),
     );
   })
   .catch((error) => {
+    console.clear()
     console.log(error);
     console.log("\nError conectando a la base de datos");
     console.log("Configura correctamente el entorno de desarrollo\n");
